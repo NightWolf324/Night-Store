17b49a6d63741c27a2718332a72a855fddd1490f
NightWolf <88887015+NightWolf324@users.noreply.github.com>
2022-09-20 22:40:30 +0700

    Add new order page

---
+++ Order/app.js
@@ -0,0 +1,51 @@
+let myLogo = document.getElementById('myLogo')
+let ytLogo = document.getElementById('youtubeLogo')
+let instaLogo = document.getElementById('instaLogo')
+let tiktokLogo = document.getElementById('tiktokLogo')
+
+let defForm = document.getElementById('defaultForm')
+let mainMenu = document.getElementById('mainMenu')
+let ytForm = document.getElementById('youtubeForm')
+
+ytLogo.style.display = "none";
+instaLogo.style.display = "none";
+tiktokLogo.style.display = "none";
+
+ytForm.style.display = "none";
+
+function milih() {
+    var mainMenu = document.getElementById('mainMenu')
+
+    if (mainMenu.value == 'yt') {
+        console.log('Youtube')
+        ytLogo.style.display = "block";
+        instaLogo.style.display = "none";
+        tiktokLogo.style.display = "none";
+        myLogo.style.display = "none"
+
+        defForm.style.display = "none"
+        ytForm.style.display = "block"
+    }
+    if (mainMenu.value == 'ig') {
+        console.log('Instagram')
+        instaLogo.style.display = "block";
+        ytLogo.style.display = "none";
+        tiktokLogo.style.display = "none";
+        myLogo.style.display = "none"
+
+        defForm.style.display = "none"
+        ytForm.style.display = "none";
+    }
+    if (mainMenu.value == 'tt') {
+        console.log('Tiktok');
+        tiktokLogo.style.display = "block";
+        ytLogo.style.display = "none";
+        instaLogo.style.display = "none";
+        myLogo.style.display = "none"
+
+        defForm.style.display = "none"
+        ytForm.style.display = "none";
+    }
+
+}
+
