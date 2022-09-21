913182494af7116162893b444ba52e958b603517
NightWolf <88887015+NightWolf324@users.noreply.github.com>
2022-09-21 12:38:55 +0700

    Add new List Order

--- Order/app.js
+++ Order/app.js
@@ -6,45 +6,58 @@ let tiktokLogo = document.getElementById('tiktokLogo')
 let defForm = document.getElementById('defaultForm')
 let mainMenu = document.getElementById('mainMenu')
 let ytForm = document.getElementById('youtubeForm')
+let igForm = document.getElementById('instaForm')
+let ttForm = document.getElementById('tiktokForm')
 
 ytLogo.style.display = "none";
 instaLogo.style.display = "none";
 tiktokLogo.style.display = "none";
 
 ytForm.style.display = "none";
+igForm.style.display = "none";
+ttForm.style.display = "none";
+
+
+
 
 function milih() {
     var mainMenu = document.getElementById('mainMenu')
 
     if (mainMenu.value == 'yt') {
         console.log('Youtube')
-        ytLogo.style.display = "block";
-        instaLogo.style.display = "none";
-        tiktokLogo.style.display = "none";
+        ytLogo.style.display = "block"
+        instaLogo.style.display = "none"
+        tiktokLogo.style.display = "none"
         myLogo.style.display = "none"
 
         defForm.style.display = "none"
         ytForm.style.display = "block"
+        igForm.style.display = "none"
+        ttForm.style.display = "none"
     }
     if (mainMenu.value == 'ig') {
         console.log('Instagram')
-        instaLogo.style.display = "block";
-        ytLogo.style.display = "none";
-        tiktokLogo.style.display = "none";
+        instaLogo.style.display = "block"
+        ytLogo.style.display = "none"
+        tiktokLogo.style.display = "none"
         myLogo.style.display = "none"
 
         defForm.style.display = "none"
-        ytForm.style.display = "none";
+        ytForm.style.display = "none"
+        igForm.style.display = "block"
+        ttForm.style.display = "none"
     }
     if (mainMenu.value == 'tt') {
         console.log('Tiktok');
-        tiktokLogo.style.display = "block";
-        ytLogo.style.display = "none";
-        instaLogo.style.display = "none";
+        tiktokLogo.style.display = "block"
+        ytLogo.style.display = "none"
+        instaLogo.style.display = "none"
         myLogo.style.display = "none"
 
         defForm.style.display = "none"
-        ytForm.style.display = "none";
+        ytForm.style.display = "none"
+        igForm.style.display = "none"
+        ttForm.style.display = "block"
     }
 
 }
